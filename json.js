6d791a9f8377e329c4ea2114521e74d269e22b96
Marylia Nieves <marylian@fullsail.edu>
2012-08-18 23:46:38 -0400

    added json file

---
+++ json.js
@@ -0,0 +1,29 @@
+var json = {
+	"task1": {
+		"checkbox": ["Choose a day", "Monday"],
+		"sub": ["Subject:", "Science"],
+		"period": ["Period:", "1"],
+		"grade": ["Grade Level:", "1"],
+		"category": ["Categories:","Grade Papers"],
+		"date": ["Due Date:", "2012-08-08"],
+		"comments": ["My Notes:", "Teacher Tasks"]
+	},
+	"task2": {
+		"checkbox": ["Choose a day", "Tuesday"],
+		"sub": ["Subject:", "Math"],
+		"period": ["Period:", "2"],
+		"grade": ["Grade Level:", "2"],
+		"category": ["Categories:","Lesson plans"],
+		"date": ["Due Date:", "2012-08-08"],
+		"comments": ["My Notes:", "To do"]
+	},
+		"task3": {
+		"checkbox": ["Choose a day", "Wednesday"],
+		"sub": ["Subject:", "Reading"],
+		"period": ["Period:", "3"],
+		"grade": ["Grade Level:", "3"],
+		"category": ["Categories:","Urgent"],
+		"date": ["Due Date:", "2012-08-08"],
+		"comments": ["My Notes:", "Teacher Tasks"]
+	}
+}
